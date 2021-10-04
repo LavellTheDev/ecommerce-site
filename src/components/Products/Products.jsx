0b1cbc9ce470bce90716ad004dc041f9367c46c7
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2021-10-03 22:33:38 -0400

    first commit

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Grid from '@material-ui/core';
+
+const products = [
+    { id:1, name: 'Shoes', description: 'Running shoes.' },
+    { id:2, name: 'Macbook', description: 'Apple Macbook.' }
+];
+
+const Products = () => {
+  <main>
+    <Grid container justify = "center" spacing = {4}>
+        {products.map((product) => (
+            <Grid item key ={product.id} xs ={12} sm={6} md={4} lg ={3}>
+                <Product />
+            </Grid>
+        ))};
+    </Grid>
+  </main>
+}
+
+export default Products;
